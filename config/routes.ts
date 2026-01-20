d3cfa9ad52d441f50150d9c07b09899e0d9b3036
Hagno1310 <hoangnh.ptit@gmail.com>
2026-01-20 15:09:03 +0700

    feat: add BT01

--- config/routes.ts
+++ config/routes.ts
@@ -43,19 +43,12 @@
 		component: './TodoList',
 	},
 
-	// DANH MUC HE THONG
-	// {
-	// 	name: 'DanhMuc',
-	// 	path: '/danh-muc',
-	// 	icon: 'copy',
-	// 	routes: [
-	// 		{
-	// 			name: 'ChucVu',
-	// 			path: 'chuc-vu',
-	// 			component: './DanhMuc/ChucVu',
-	// 		},
-	// 	],
-	// },
+	{
+		path: '/danh-muc-san-pham',
+		name: 'Danh mục sản phẩm',
+		icon: 'OrderedListOutlined',
+		component: './DanhMucSanPham',
+	},
 
 	{
 		path: '/notification',
